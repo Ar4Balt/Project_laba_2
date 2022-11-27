fed9da00d293320861592db6aa540841831efe41
Tunnikov Artur <tunnikovartur@gmail.com>
2022-11-27 23:00:31 +0300

    Creating two data files for salad and vegetables

--- data_files/WriteToTheFile.java
+++ data_files/WriteToTheFile.java
@@ -14,13 +14,29 @@ public class WriteToTheFile {
 
     public WriteToTheFile(ArrayList<Vegetables> vegetables) {
         Salad salad = new Salad(vegetables);
+        /*
+        * You need to change the path or remove it altogether
+        * */
+        String thePathToTheFile = "D:\\VS Code\\Programms\\Java\\Project_laba_2" +
+                "\\src\\by\\Ar4Balt\\tunnikov\\project_laba_2\\data_files\\";
         try {
-            File file = new File("D:\\VS Code\\Programms\\Java\\Project_laba_2\\src\\by\\Ar4Balt\\tunnikov\\project_laba_2\\data_files\\dataFile.txt");
-            if (!file.exists()) {
-                file.createNewFile();
+            File fileSalad = new File(thePathToTheFile + "dataSaladFile.txt");
+            if (!fileSalad.exists()) {
+                fileSalad.createNewFile();
             }
-            PrintWriter printWriter = new PrintWriter(file);
+            PrintWriter printWriter = new PrintWriter(fileSalad);
             printWriter.println(salad);
+            printWriter.close();
+        } catch (IOException error) {
+            System.out.println("Error: " + error);
+        }
+        try {
+            File fileVegetables = new File(thePathToTheFile + "dataVegetablesFile.txt");
+            if (!fileVegetables.exists()) {
+                fileVegetables.createNewFile();
+            }
+            PrintWriter printWriter = new PrintWriter(fileVegetables);
+            printWriter.println(vegetables);
             printWriter.close();
         } catch (IOException error) {
             System.out.println("Error: " + error);
