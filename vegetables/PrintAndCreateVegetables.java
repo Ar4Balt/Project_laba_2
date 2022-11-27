b3848e9666b1a9d2f2bd1c1aebdc009e101a7738
Tunnikov Artur <tunnikovartur@gmail.com>
2022-11-27 06:13:35 +0300

    Change Potato insted of Broccoli and Cabbage

--- vegetables/PrintAndCreateVegetables.java
+++ vegetables/PrintAndCreateVegetables.java
@@ -7,12 +7,16 @@ import java.util.ArrayList;
 public class PrintAndCreateVegetables {
     public PrintAndCreateVegetables() {
         ArrayList<Vegetables> vegetables = new ArrayList<Vegetables>();
-        vegetables.add(new Potato());
-        vegetables.add(new Potato());
-        for (int i = 0; i < vegetables.size(); i++) {
-            System.out.println(i + "): \n"
-                    + vegetables.get(i));
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        switch (scanner){
+//
+//        }
+        vegetables.add(new Broccoli());
+        vegetables.add(new Cabbage());
+//        for (int i = 0; i < vegetables.size(); i++) {
+//            System.out.println(i + "): \n"
+//                    + vegetables.get(i));
+//        }
         PrintAndCreateSalad salad = new PrintAndCreateSalad(vegetables);
 
     }
