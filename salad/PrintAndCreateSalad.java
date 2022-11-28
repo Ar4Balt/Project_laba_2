e6a955a6b662b963859f00c7a2d8130c2f968f36
Tunnikov Artur <tunnikovartur@gmail.com>
2022-11-28 04:02:24 +0300

    Add constructer with String

--- salad/PrintAndCreateSalad.java
+++ salad/PrintAndCreateSalad.java
@@ -2,12 +2,17 @@ package by.Ar4Balt.tunnikov.project_laba_2.salad;
 
 
 import by.Ar4Balt.tunnikov.project_laba_2.vegetables.Vegetables;
-
 import java.util.ArrayList;
 
-public class PrintAndCreateSalad {
+public class PrintAndCreateSalad extends Salad{
+    public PrintAndCreateSalad() {}
+
     public PrintAndCreateSalad(ArrayList<Vegetables> vegetables) {
         Salad salad = new Salad(vegetables);
         System.out.println(salad);
+    }
+
+    public PrintAndCreateSalad(String saladName) {
+        super(saladName);
     }
 }
