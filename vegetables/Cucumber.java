a93f251f24ebbfe7d548cfafc3d14dc22a66adb0
Tunnikov Artur <tunnikovartur@gmail.com>
2022-11-28 04:08:20 +0300

    Rename (switch 'c'-ru to 'c'-en)

--- vegetables/Cucumber.java
+++ vegetables/Cucumber.java
@@ -1,7 +1,7 @@
 package by.Ar4Balt.tunnikov.project_laba_2.vegetables;
 
-public class Сucumber extends Vegetables {
-    public Сucumber() {
+public class Cucumber extends Vegetables {
+    public Cucumber() {
         super("Cucumber", 13, 100);
     }
 }
